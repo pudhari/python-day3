00ec6563f5e5878c12378eee00b33a74e0ef0f30
pudhari <sai.priya.pudhari.2025@sse.ac.in>
2025-12-10 23:57:20 +0530

    Add pip install command for plotly
    Added installation command for plotly.

--- ATCS_basic.py
+++ ATCS_basic.py
@@ -14,6 +14,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
+pip install plotly
 
 st.set_page_config(page_title="ATCS Simulator (Intermediate)", layout="wide")
 
